1a51da0eec79815d50b7bbe86b53183201aa6429
poopman <kylecypher@gmail.com>
2023-05-31 15:15:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -165,7 +165,6 @@ function AskForSurfaceData() {
 
 function AskForWorldData(x, y) {
   if (user) {
-
     tellServer({
       AskForWorldData: true,
       location: [x, y, tileW, tileH, cellW, cellH],
@@ -251,7 +250,7 @@ const asyncGetTiles = async () => {
 
   setTimeout(function() {
     asyncGetTiles()
-  }, 1000)
+  }, 5000)
 }
 
 
@@ -495,16 +494,19 @@ const materialKey = {
   "11204863": MinecraftImageSource.diamond,
   "16711680": MinecraftImageSource.lava,
 }
-//                       grass  dirt   gravel    mud      sand     clay    stone     iron   copper    gold    diamond    lava  unbreakable
-const blockCharColors = [32768,8281926,11316396,4271395,16772002,13418174,8289918,12763344,13801216,16766976,11204863,16711680,0];
-const blockCharText = ["«","Ò","Ö","Õ","Ö","Õ","Ñ","®","²","Ô","Ô","®","¬"];
-const blockPrimary = ["#7e5f46", "#7e5f46", "#acacac", "#412d23", "#ffeba2", "#cfb7b7", "#7e7e7e", "#d0cde4", "#d29700", "#acacac", "#acacac", "#fe0001" , "#608dad"];
-const blockSecondary = ["#008000", "#76573e", "#7e7e7e", "#4f3426", "#e0c978", "#a1846d", "#777373", "#b4b1cd", "#acacac", "#ffd800", "#aaf8ff", "#8a0606", "#c6d7e3"]
+//                       grass  dirt   gravel    mud      sand     clay    stone     iron   copper    gold    diamond    lava  unbreakable brick
+const blockCharColors = [32768,8281926,11316396,4271395,16772002,13418174,8289918,12763344,13801216,16766976,11204863,16711680,0,13047552];
+const blockCharText = ["«","Ò","Ö","Õ","Ö","Õ","Ñ","®","²","Ô","Ô","®","¬","°"];
+const blockPrimary = ["#7e5f46", "#7e5f46", "#acacac", "#412d23", "#ffeba2", "#cfb7b7", "#7e7e7e", "#d0cde4", "#d29700", "#acacac", "#acacac", "#fe0001" , "#608dad","#C71700"];
+const blockSecondary = ["#008000", "#76573e", "#7e7e7e", "#4f3426", "#e0c978", "#a1846d", "#777373", "#b4b1cd", "#acacac", "#ffd800", "#aaf8ff", "#8a0606", "#c6d7e3","#ffffff"]
+
 const playerchars = ["¤","¡","▇","▉","ð","í","ö","÷","ù","ú","û","ý","þ","ø","§","Ã","Í","Ä","ë","ò","¢","£","Æ","Ë","¾","¿","ä","å","È","É","Á","ç","Â","è"];
 const playerColors = ["#ecc983", "#ecc983","#000","#000","#005aff","#005aff","#005aff","#005aff","#005aff","#005aff","#005aff","#005aff","#005aff","#005aff","#6f6f6f","#6f6f6f","#6f6f6f","#6f6f6f","#6f6f6f","#6f6f6f","#6f6f6f","#6f6f6f","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#ecc983","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000","#000",]
 for (block in Object.keys(materialKey)) {
   charImages.push(new Image)
 }
+
+
 const getMaterialIndex = (number) => {
   const keys = Object.keys(materialKey).sort((a, b) => Number(a) - Number(b));
   const key = keys.find(key => Number(key) === number);
